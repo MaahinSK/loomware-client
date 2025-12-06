620709d368a6df6bc532e8b7c7ad558932d644ca
MaahinSK <maahin810@gmail.com>
2025-12-07 01:44:41 +0600

    sdsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,97 @@
-export default function App() { return <div>App</div>; }
+export default function App() { return <div>App</div>; } import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AOS from 'aos';
+import Layout from './components/common/Layout';
+import Home from './pages/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import AllProductsPage from './pages/AllProductsPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+import AboutUs from './pages/AboutUs';
+import Contact from './pages/Contact';
+import Dashboard from './pages/Dashboard';
+import NotFound from './pages/NotFound';
+import PrivateRoute from './components/common/PrivateRoute';
+
+function App() {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            once: true,
+            offset: 100,
+        });
+    }, []);
+
+    return (
+        <Router>
+            <Routes>
+                {/* Public Routes */}
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="products" element={<AllProductsPage />} />
+                    <Route path="products/:id" element={<ProductDetailsPage />} />
+                    <Route path="about" element={<AboutUs />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+
+                    {/* Private Routes */}
+                    <Route path="dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+
+                    {/* 404 Route */}
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
+}
+
+export default App; import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AOS from 'aos';
+import Layout from './components/common/Layout';
+import Home from './pages/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import AllProductsPage from './pages/AllProductsPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+import AboutUs from './pages/AboutUs';
+import Contact from './pages/Contact';
+import Dashboard from './pages/Dashboard';
+import NotFound from './pages/NotFound';
+import PrivateRoute from './components/common/PrivateRoute';
+
+function App() {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            once: true,
+            offset: 100,
+        });
+    }, []);
+
+    return (
+        <Router>
+            <Routes>
+                {/* Public Routes */}
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="products" element={<AllProductsPage />} />
+                    <Route path="products/:id" element={<ProductDetailsPage />} />
+                    <Route path="about" element={<AboutUs />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+
+                    {/* Private Routes */}
+                    <Route path="dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+
+                    {/* 404 Route */}
+                    <Route path="*" element={<NotFound />} />
+                </Route>
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 import React from 'react'; import ReactDOM from 'react-dom'; import App from './App'; ReactDOM.render(<App />, document.getElementById('root'));
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
+import { HelmetProvider } from 'react-helmet-async';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'aos/dist/aos.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'leaflet/dist/leaflet.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <HelmetProvider>
+            <AuthProvider>
+                <App />
+                <ToastContainer
+                    position="top-right"
+                    autoClose={3000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="colored"
+                />
+            </AuthProvider>
+        </HelmetProvider>
+    </React.StrictMode>
+);
